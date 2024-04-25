83230ed08f884620526b1368ef7cba8bf629f464
muni0143 <muniranganath1810@gmail.com>
2024-04-25 19:28:28 +0530

    my first commit

---
+++ loginform/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+import LoginForm from './Components/Assets/LoginForm/LoginForm';
+
+function App() {
+  return (
+    <div >
+    <LoginForm />
+      
+    </div>
+  );
+}
+
+export default App;
